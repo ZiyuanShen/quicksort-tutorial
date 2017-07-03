f36db707ffcf182ff69919d6e724051956a308ad
Matthew Berryman <matthew@acrossthecloud.net>
2017-07-03 13:52:36 +1000

    template

---
+++ main.js
@@ -0,0 +1,27 @@
+
+function swap(items, firstIndex, secondIndex){
+    var temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
+
+function partition(items, left, right) {
+
+    var pivot   = items[Math.floor((right + left) / 2)],
+        i       = left,
+        j       = right;
+
+    // complete_me
+
+    return i;
+}
+
+
+function quickSort(items, left, right) {
+
+    var index;
+
+    // complete_me
+
+    return items;
+}
